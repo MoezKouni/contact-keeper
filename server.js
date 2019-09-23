db33d430b54d64371d7993106c3a3e0072db48a5
MoezKouni <mo.elkouni@gmail.com>
2019-09-23 14:26:11 +0100

    config folder

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express()
-const PORT = process.env.PORT || 5000
+
+// Connect DataBase
+connectDB()
+
+// Init Middleware
+app.use(express.json({extended: false})) // body-parser is included in express
+
+
+
+app.get('/', (req, res) => {
+    res.json({msg: 'Hello'})
+})
 
 // Define Routes
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
 
-app.get('/', (req, res) => {
-    res.json({msg: 'Hello'})
-})
 
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`))
